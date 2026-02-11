8e0d35c6baccd877d800082bd7544f087cdcf860
alex misyuk <glassfox@gmail.com>
2026-02-11 14:21:41 +0200

    Add ModuleGen package metadata for catalog common

---
+++ opinion-catalog-common/src/main/java/com/inqwise/opinion/catalog/common/package-info.java
@@ -0,0 +1,4 @@
+@ModuleGen(name = "opinion-catalog-common", groupPackage = "com.inqwise.opinion.catalog.common")
+package com.inqwise.opinion.catalog.common;
+
+import io.vertx.codegen.annotations.ModuleGen;
